a0a914950f6ff64ba0495c43d84baa5f7574b7f0
mohdshahzad1234 <iamshahzad68@gmail.com>
2025-05-25 19:12:17 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,28 @@
+class Settings:
+	def __init__(self):
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230,230,230)
+		self.bullet_width = 300
+		self.bullet_height = 10
+		self.bullet_color=(60,60,60)
+		self.bullets_allowed = 3
+		self.fleet_drop_speed = 10
+		self.initialize_dynamic_settings()
+		self.speedup_scale = 1.1
+		self.score_scale = 1.5
+		self.ship_limit = 3
+
+	def initialize_dynamic_settings(self):
+		self.ship_speed = 3.0
+		self.bullet_speed = 3.0
+		self.alien_speed = 3.0
+		self.fleet_direction = 1
+		self.alien_points = 50
+
+
+	def increase_speed(self):
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
+		self.alien_points = int(self.alien_points * self.score_scale)
